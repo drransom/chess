834a98bbc8bdbcd5e5686ea22473546999751adf
Elliot Reed <elliotreed2@gmail.com>
2015-07-20 15:03:52 -0700

    change styling in game.rb

--- lib/game.rb
+++ lib/game.rb
@@ -23,99 +23,99 @@ class Game
 
   private
 
-    def initialize_game
-      puts "Welcome to chess."
-    end
+  def initialize_game
+    puts "Welcome to chess."
+  end
 
-    def play_game
-      until game_over?
-        @board.reset_en_passant(@current_player.color)
-        display_board
-        begin
-          move = @moves.empty? ? @current_player.play_turn : @moves.shift
-          if move[0].downcase == 'q'
-            move = @current_player.confirm_quit
-            if move[0] && move[0].downcase == 'y'
-              return :quit
-            end
-          end
-          promote_pawn(move) if try_move_piece(move)
-        rescue => e
-          puts e.message
-          retry
-        end
-        flip_current_player
-      end
-    end
-
-    #returns whether a pawn needs to be promoted
-    def try_move_piece(move)
-      raise InputError.new unless valid_format?(move)
-      from = translate_move_notation(move[0..1])
-      to = translate_move_notation(move[-2..-1])
-      raise PieceNotOwnedError.new if !@board.occupied?(from) ||
-        @board.color(from) != @current_player.color
-      raise IllegalMoveError.new unless @board.move_legal?(from, to)
-      raise CheckError.new if @board.leaves_self_in_check?(from, to, @current_player.color)
-      @board.move_piece(from, to)
-    end
-
-    def valid_format?(move)
-      move.match(/\A[a-hA-H][1-8][, ]+[a-hA-H][1-8]\Z/)
-    end
-
-    def translate_move_notation(string)
-      row = 8 - string[1].to_i
-      col = string[0].ord - 97
-      [row, col]
-    end
-
-    def promote_pawn(move)
+  def play_game
+    until game_over?
+      @board.reset_en_passant(@current_player.color)
       display_board
       begin
-        piece = @current_player.request_pawn.to_sym
-        raise PromotePawnError.new unless [:bishop, :knight, :queen, :rook].include?(piece)
+        move = @moves.empty? ? @current_player.play_turn : @moves.shift
+        if move[0].downcase == 'q'
+          move = @current_player.confirm_quit
+          if move[0] && move[0].downcase == 'y'
+            return :quit
+          end
+        end
+        promote_pawn(move) if try_move_piece(move)
       rescue => e
         puts e.message
         retry
       end
-      new_position = (move.split.map { |e| e }).last
-      @board.promote_pawn(piece, translate_move_notation(new_position))
+      flip_current_player
     end
+  end
 
-    def flip_current_player
-      @current_player = @current_player == @white_player ? @black_player : @white_player
-    end
+  #returns whether a pawn needs to be promoted
+  def try_move_piece(move)
+    raise InputError.new unless valid_format?(move)
+    from = translate_move_notation(move[0..1])
+    to = translate_move_notation(move[-2..-1])
+    raise PieceNotOwnedError.new if !@board.occupied?(from) ||
+      @board.color(from) != @current_player.color
+    raise IllegalMoveError.new unless @board.move_legal?(from, to)
+    raise CheckError.new if @board.leaves_self_in_check?(from, to, @current_player.color)
+    @board.move_piece(from, to)
+  end
 
-    def game_over?
-      @board.checkmate?(@current_player.color) || @board.stalemate?(@current_player.color)
-    end
+  def valid_format?(move)
+    move.match(/\A[a-hA-H][1-8][, ]+[a-hA-H][1-8]\Z/)
+  end
 
-    def display_board
-      puts @board.display
-    end
+  def translate_move_notation(string)
+    row = 8 - string[1].to_i
+    col = string[0].ord - 97
+    [row, col]
+  end
 
-    def display_result(result)
-      if result == :quit
-        puts "You have quit the program."
-      else
-        display_winner
-      end
+  def promote_pawn(move)
+    display_board
+    begin
+      piece = @current_player.request_pawn.to_sym
+      raise PromotePawnError.new unless [:bishop, :knight, :queen, :rook].include?(piece)
+    rescue => e
+      puts e.message
+      retry
     end
+    new_position = (move.split.map { |e| e }).last
+    @board.promote_pawn(piece, translate_move_notation(new_position))
+  end
 
-    def display_winner
-      display_board
-      if @board.stalemate?(@current_player.color)
-        puts "Stalemate."
-      else
-        flip_current_player
-        puts "#{@current_player.color} wins!"
-      end
-    end
+  def flip_current_player
+    @current_player = @current_player == @white_player ? @black_player : @white_player
+  end
 
-    def self.make_moves_from_file(filename)
-      @moves = File.readlines(filename).map(&:chomp)
+  def game_over?
+    @board.checkmate?(@current_player.color) || @board.stalemate?(@current_player.color)
+  end
+
+  def display_board
+    puts @board.display
+  end
+
+  def display_result(result)
+    if result == :quit
+      puts "You have quit the program."
+    else
+      display_winner
     end
+  end
+
+  def display_winner
+    display_board
+    if @board.stalemate?(@current_player.color)
+      puts "Stalemate."
+    else
+      flip_current_player
+      puts "#{@current_player.color} wins!"
+    end
+  end
+
+  def self.make_moves_from_file(filename)
+    @moves = File.readlines(filename).map(&:chomp)
+  end
 end
 
 class HumanPlayer
@@ -141,7 +141,6 @@ class HumanPlayer
     gets.downcase.chomp
   end
 end
-
 
 if __FILE__ == $0
   filename = ARGV.shift
