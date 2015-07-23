efd825923ce4de86a79c381a80bc3c16a86ff6c7
Elliot Reed <elliotreed2@gmail.com>
2015-07-22 23:16:33 -0700

    edit ChessHistory file, all specs pass

--- lib/chess_history.rb
+++ lib/chess_history.rb
@@ -4,26 +4,15 @@ require_relative 'pieces'
 
 class ChessHistory
   def initialize
-    @game_states = []
-    @three_repeats = false
+    @boards = []
   end
 
-  def update_history(game)
-    @game_states.push(ChessGameState.new(game))
+  def update_history(board)
+    @boards.push(board)
   end
 
   def three_repeats?
-    @game_states.select { |board| board == @boards.last }.length >= 3
-  end
-
-end
-
-class ChessGameState
-  def initialize(game)
-    @board = game.board
-    @en_passant_capture = game.has_en_passant_capture
-    @can_castle_white = @board.can_castle(:white)
-    @can_castle_black = @board.can_castle(:black)
+    @boards.select { |board| board == @boards.last }.length >= 3
   end
 
 end
