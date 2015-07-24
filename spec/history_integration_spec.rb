cac54eee7ee4b40ad922a45ada0f8f696ec50d12
Elliot Reed <elliotreed2@gmail.com>
2015-07-24 13:06:07 -0700

    create history specs and all fail for correct reason

--- spec/history_integration_spec.rb
+++ spec/history_integration_spec.rb
@@ -54,6 +54,52 @@ context "chess history integration" do
     play_test_game(moves)
   end
 
-  it ""
+  it "rooks that have moved differently are the same after a fourth iteration" do
+    moves = File.readlines('spec/moves/one_moving_rook_four_repeats.txt').map(&:chomp)
+    game.send(:initialize_game, moves)
+    expect(white_player).to receive(:request_three_move_stalemate).once.and_return('n')
+    expect(black_player).to receive(:request_three_move_stalemate).once.and_return('y')
+    play_test_game(moves)
+  end
 
+  it "three iterations are not a repeat if en passant was available the first time" do
+    moves = File.readlines('spec/moves/en_passant_three_repeats.txt').map(&:chomp)
+    game.send(:initialize_game, moves)
+    expect(white_player).not_to receive(:request_three_move_stalemate)
+    expect(black_player).not_to receive(:request_three_move_stalemate)
+    play_test_game(moves)
+  end
+
+  it "four iterations are a repeat if en passant was available the first time" do
+    moves = File.readlines('spec/moves/en_passant_four_repeats.txt').map(&:chomp)
+    game.send(:initialize_game, moves)
+    expect(white_player).to receive(:request_three_move_stalemate).once.and_return('n')
+    expect(black_player).to receive(:request_three_move_stalemate).once.and_return('y')
+    play_test_game(moves)
+  end
+
+  it "three iterations are not a repeat if the king moves" do
+    moves = File.readlines('spec/moves/king_move_three_repeats.txt').map(&:chomp)
+    game.send(:initialize_game, moves)
+    expect(white_player).not_to receive(:request_three_move_stalemate)
+    expect(black_player).not_to receive(:request_three_move_stalemate)
+    play_test_game(moves)
+  end
+
+  it "four iterations are a repeat if the king moved the first time" do
+    moves = File.readlines('spec/moves/king_move_four_repeats.txt').map(&:chomp)
+    game.send(:initialize_game, moves)
+    expect(white_player).to receive(:request_three_move_stalemate).once.and_return('n')
+    expect(black_player).to receive(:request_three_move_stalemate).once.and_return('y')
+    play_test_game(moves)
+  end
+
+  it "repeat requires the same player to move" do
+    moves = File.readlines('spec/moves/three_repeats_different_player_moving').map(&:chomp)
+    game.send(:initialize_game, moves)
+    expect(white_player).not_to receive(:request_three_move_stalemate)
+    expect(black_player).not_to receive(:request_three_move_stalemate)
+    play_test_game(moves)
+
+  end
 end
